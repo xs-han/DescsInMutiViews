b7a3c68ee321f1dbfcf47a18514c58b4058361d5
xushen <hanxushen1105@sina.com>
2017-09-25 03:15:40 +0800

    add history function for tracker

--- FeatureTracker.cpp
+++ FeatureTracker.cpp
@@ -5,7 +5,7 @@
 #include "FeatureTracker.h"
 #include <ctime>
 
-FeatureTracker::FeatureTracker(string vName) {
+FeatureTracker::FeatureTracker(string vName):historySize(10) {
     videoName = vName;
     vs.setVideo(vName);
     vs.setVideoWidth(640);
@@ -18,63 +18,92 @@ void FeatureTracker::track() {
     namedWindow(videoName, 0);
     namedWindow("Matches", 0);
     namedWindow("MatchWithInit", 0);
-    if(initFrame.empty()){
-        vs >> initFrame;
-        fe.extract(initFrame, initKps, initDescs);
-        lastFrame = initFrame.clone();
-        lastKps = initKps;
-        lastDescs = initDescs.clone();
-        fm.match(initKps, initDescs, lastKps, lastDescs, matches4Init);
-    }
+
     Mat matchFrame;
     Mat matchInitFrame;
     while(vs >> curtFrame){
+        if(initFrame.empty()){
+            historicalFrame.clear();
+            historicalKps.clear();
+            historicalDescs.clear();
+            matchesHistoryAndInit.clear();
+            matchesCurtAndHistory.clear();
+            matchesCurtAndInit.clear();
+            curtKps.clear();
+            initKps.clear();
+
+            initFrame = curtFrame.clone();
+            fe.extract(initFrame, initKps, initDescs);
+            matchesCurtAndHistory.resize(historySize);
+            matchesCurtAndInit.resize(historySize);
+            for(int i = 0; i < historySize; i++){
+                Mat tmpFrame;
+                vector<KeyPoint> tmpKps;
+                Mat tmpDescs;
+                vector<DMatch> tmpMatch4Init;
+
+                vs >> tmpFrame;
+                fe.extract(tmpFrame, tmpKps, tmpDescs);
+                historicalFrame.push_front(tmpFrame.clone());
+                historicalKps.push_front(tmpKps);
+                historicalDescs.push_front(tmpDescs.clone());
+
+                fm.match(tmpKps, tmpDescs, initKps, initDescs, tmpMatch4Init);
+                matchesHistoryAndInit.push_front(tmpMatch4Init);
+            }
+        }
         //clock_t begin = clock();
-        imshow(videoName, curtFrame);
         int key = cvWaitKey((int)(vs.getFrameRate()+0.5) - 20);
+        imshow(videoName, curtFrame);
         switch (key){
+            case -1:
+            {
+                break;
+            }
             case 's':
             {
-                initFrame = curtFrame.clone();
-                fe.extract(initFrame, initKps, initDescs);
-                lastFrame = initFrame.clone();
-                lastKps = initKps;
-                lastDescs = initDescs.clone();
-                fm.match(initKps, initDescs, lastKps, lastDescs, matches4Init);
-                break;
+                initFrame.resize(0);
+                continue;
             }
             case 'p':
             {
                 cvWaitKey(0);
                 break;
             }
-            case 'e':
+            case 'q':
             {
                 return;
             }
             default:
             {
-
+                cout << "s -> start, q -> quit, p -> pause." << endl;
             }
         }
         fe.extract(curtFrame, curtKps, curtDescs);
         //cout << "extract: " << double(clock() - begin) / CLOCKS_PER_SEC << endl;
-        fm.match(lastKps, lastDescs, curtKps, curtDescs, matches4CurtFrame);
+        for(int i = 0; i < historySize; i++){
+            vector<DMatch> tmpMatch4Curt;
+            fm.match( curtKps, curtDescs, historicalKps[i], historicalDescs[i],tmpMatch4Curt);
+            matchesCurtAndHistory[i] = tmpMatch4Curt;
+        }
+
         //cout << "match: " << double(clock() - begin) / CLOCKS_PER_SEC << endl;
-        cv::drawMatches(lastFrame, lastKps, curtFrame, curtKps, matches4CurtFrame, matchFrame);
+        cv::drawMatches(curtFrame, curtKps, historicalFrame[0], historicalKps[0], matchesCurtAndHistory[0], matchFrame);
         imshow("Matches", matchFrame);
 
-        vector<DMatch> tmp;
-        forwardMatches(initDescs, curtDescs, matches4Init, matches4CurtFrame, tmp);
-        matches4Init = tmp;
-        cout << tmp[0].distance << endl;
-        drawMatches(initFrame, initKps, curtFrame, curtKps, matches4Init, matchInitFrame);
+        for(int i = 0; i < historySize; i++){
+            forwardMatches(curtDescs, initDescs, matchesCurtAndHistory[i], matchesHistoryAndInit[i], matchesCurtAndInit[i]);
+        }
+        mergeMatches(curtDescs, initDescs, matchesCurtAndInit, mergedMatchesCurtAndInit);
+        drawMatches(curtFrame, curtKps, initFrame, initKps, mergedMatchesCurtAndInit, matchInitFrame);
         imshow("MatchWithInit", matchInitFrame);
 
-        lastFrame = curtFrame.clone();
-        lastKps.clear();
-        lastKps = curtKps;
-        lastDescs = curtDescs.clone();
+        cout << averageDistanse(mergedMatchesCurtAndInit) << endl;
+
+        pushQueue(historicalFrame, curtFrame.clone());
+        pushQueue(historicalKps, curtKps);
+        pushQueue(historicalDescs, curtDescs.clone());
+        pushQueue(matchesHistoryAndInit, mergedMatchesCurtAndInit);
         //cout << "show: " << double(clock() - begin) / CLOCKS_PER_SEC << endl;
     }
 }
@@ -93,19 +122,32 @@ void FeatureTracker::forwardMatches(Mat &descA, Mat &descC, vector<DMatch> &matc
     }
 }
 
-//void FeatureTracker::MergeMatches(vector<DMatch> &matchABOld, vector<DMatch> &matchABNew,
-//                                    vector<DMatch> &matchAB) {
-//    matchAB.clear();
-//    for(int i = 0; i < matchABOld.size(); i++ ){
-//        for(int j = 0; j < matchABNew.size(); j++){
-//            if(matchABOld[i].trainIdx == matchABNew[j].queryIdx){
-//                float dis = static_cast<float>(calL2Distance(descA.row(i), descC.row(j)));
-//                matchAC.push_back(DMatch(matchAB[i].queryIdx, matchBC[j].trainIdx, dis));
-//                continue;
-//            }
-//        }
-//    }
-//}
+void FeatureTracker::mergeMatches(Mat &descA, Mat &descB, vector<vector<DMatch> > &multiMatchesAB,
+                                  vector<DMatch> &matchAB) {
+    matchAB.clear();
+    const int numBDescs = descB.rows;
+    int existMatchInB[numBDescs];
+    for(int i = 0; i < numBDescs; i++){
+        existMatchInB[i] = -1;
+    }
+
+    for(int i = 0; i < multiMatchesAB.size(); i++ ){
+        const vector<DMatch> & anImageMatchPair = multiMatchesAB[i];
+        for(int j = 0; j < anImageMatchPair.size(); j++){
+            const DMatch & aMatch = anImageMatchPair[j];
+            if(existMatchInB[aMatch.trainIdx] == -1){
+                existMatchInB[aMatch.trainIdx] = aMatch.queryIdx;
+            }
+        }
+    }
+
+    for(int i = 0; i < numBDescs; i++){
+        if(existMatchInB[i] != -1){
+            float dis = static_cast<float>(calL2Distance(descA.row(existMatchInB[i]), descB.row(i)));
+            matchAB.push_back(DMatch(existMatchInB[i], i, dis));
+        }
+    }
+}
 
 double FeatureTracker::calL2Distance(const Mat &a, const Mat &b) {
     assert(a.rows == 1 && b.rows == 1 && a.cols == b.cols);
@@ -114,5 +156,19 @@ double FeatureTracker::calL2Distance(const Mat &a, const Mat &b) {
         dis += (a.at<float>(1,i) - b.at<float>(1,i)) * (a.at<float>(1,i) - b.at<float>(1,i));
     }
     return sqrt(dis);
+}
+
+double FeatureTracker::averageDistanse(const vector<DMatch> & match) {
+    double dis = 0;
+    for (int i = 0; i < match.size(); ++i) {
+        dis += match[i].distance;
+    }
+    return dis / match.size();
+}
+
+template<typename T>
+void FeatureTracker::pushQueue(deque<T> &queue, const T &anElement) {
+    queue.push_front(anElement);
+    queue.pop_back();
 }
 
--- FeatureTracker.h
+++ FeatureTracker.h
@@ -11,6 +11,8 @@
 #include "FeatureExtractor.h"
 
 class FeatureTracker {
+    int historySize = 20;
+
     string videoName;
     VideoStream vs;
     FeatureMatcher fm;
@@ -20,22 +22,29 @@ class FeatureTracker {
     vector<KeyPoint> curtKps;
     Mat curtDescs;
 
-    Mat lastFrame;
-    vector<KeyPoint> lastKps;
-    Mat lastDescs;
+    deque<Mat> historicalFrame;
+    deque<vector<KeyPoint> > historicalKps;
+    deque<Mat> historicalDescs;
 
     Mat initFrame;
     vector<KeyPoint> initKps;
     Mat initDescs;
 
-    vector<DMatch> matches4Init;
-    vector<DMatch> matches4CurtFrame;
+    deque<vector<DMatch> > matchesHistoryAndInit;
+    vector<vector<DMatch> > matchesCurtAndHistory;
+    vector<vector<DMatch> > matchesCurtAndInit;
+    vector<DMatch> mergedMatchesCurtAndInit;
 
 public:
     explicit FeatureTracker(string vName);
     void track();
     void forwardMatches(Mat &A, Mat &C, vector<DMatch> &matchAB, vector<DMatch> &matchBC, vector<DMatch> &matchAC);
+    void mergeMatches(Mat &descA, Mat &descB, vector<vector<DMatch>> &multiMatchesAB, vector<DMatch> &matchAB);
     double calL2Distance(const Mat & a, const Mat & b);
+    template<typename T>
+    void pushQueue(deque<T> & queue, const T & anElement);
+
+    double averageDistanse(const vector<DMatch> &match);
 };
 
 
